ac46292a66f77e4d0c7e3e6aabdc9a0445f754d7
Aleksey Trubitsyn <at.8441451@yandex.ru>
2019-06-09 20:41:57 +0300

    create MainPage view, add to the App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
+import MainPage from './views/main-page';
+
 function App() {
   return (
-    <div className="App">
-      <h1>START</h1>
-    </div>
+    <MainPage />
   );
 }
 
---
+++ src/views/main-page/main-page.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Page from '../page';
+
+const MainPage = () => (
+  <Page
+    mix="main-page"
+    title="Список сотрудников"
+  >
+    <h2 className="main-page__temp-title">
+      {'На этом месте будет список сотрудников'}
+    </h2>
+  </Page>
+);
+
+export default MainPage;
