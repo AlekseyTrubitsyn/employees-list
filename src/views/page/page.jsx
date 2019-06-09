557c1cae077fcdb86b7e259fe12dc38ee943f7ae
Aleksey Trubitsyn <at.8441451@yandex.ru>
2019-06-09 20:41:46 +0300

    create Page view

---
+++ src/views/page/page.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import PageWrapper from '../../components/page-wrapper';
+import PageContent, { headerPropTypes } from '../../components/page-content';
+import PageFooter from '../../components/page-footer';
+
+const propTypes = {
+  ...headerPropTypes,
+  mix: PropTypes.string,
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ]).isRequired,
+};
+
+const defaultProps = {
+  mix: '',
+};
+
+const Page = ({ mix, link, title, children }) => (
+  <div className={`${mix} page`.trim()}>
+    <div className="page__content">
+      <PageWrapper>
+        <PageContent
+          title={title}
+          link={link}
+        >
+          {children}
+        </PageContent>
+      </PageWrapper>
+    </div>
+    <PageFooter />
+  </div>
+);
+
+Page.propTypes = propTypes;
+Page.defaultProps = defaultProps;
+export default Page;
