d761047e78d3ca29dad6e87e534a5f0637808cf5
Aleksey Trubitsyn <at.8441451@yandex.ru>
2019-06-10 19:34:34 +0300

    add EmployeesList container to the MainPage

--- src/views/main-page/main-page.jsx
+++ src/views/main-page/main-page.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 
 import Page from '../page';
+import EmployeesListContainer from '../../containers/employees-list-container';
 
 const MainPage = () => (
   <Page
     mix="main-page"
     title="Список сотрудников"
   >
-    <h2 className="main-page__temp-title">
-      {'На этом месте будет список сотрудников'}
-    </h2>
+    <EmployeesListContainer />
   </Page>
 );
 
