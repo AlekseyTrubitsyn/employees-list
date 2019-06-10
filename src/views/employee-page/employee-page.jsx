01371ec6c249fdca14ed567930541e138f8a6213
Aleksey Trubitsyn <at.8441451@yandex.ru>
2019-06-10 22:58:08 +0300

    add EmployeeContainer to the EmployeePage

--- src/views/employee-page/employee-page.jsx
+++ src/views/employee-page/employee-page.jsx
@@ -1,20 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import Page from '../page';
+import EmployeeContainer from '../../containers/employee-container';
 
-const EmployeePage = ({ match }) => (
-  <Page
-    mix="employee-page"
-    title={`Данные сотрудника ID #${match.params.id}`}
-    link={{
-      href: '/',
-      text: 'К списку'
-    }}
-  >
-    <h2 className="main-page__temp-title">
-      {`На этом месте будут данные сотрудника c ID #${match.params.id}`}
-    </h2>
-  </Page>
-);
+const propsTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
 
+const EmployeePage = ({ match }) => {
+  const id = match.params.id;
+
+  return (
+    <Page
+      mix="employee-page"
+      title={`Данные сотрудника ID #${id}`}
+      link={{
+        href: '/',
+        text: 'К списку'
+      }}
+    >
+      <EmployeeContainer id={id} />
+    </Page>
+  );
+}
+
+EmployeePage.propsTypes = propsTypes;
 export default EmployeePage;
