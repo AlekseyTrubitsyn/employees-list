8021ee13cc37f44123ab87e4001409351c64c645
Aleksey Trubitsyn <at.8441451@yandex.ru>
2019-06-10 22:56:10 +0300

    create EmployeeErrorMessage component

---
+++ src/components/employee-error-message/employee-error-message.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const EmployeeErrorMessage = () => (
+  <h2 className="employee-error-message">
+    {'Сотрудник не найден'}
+  </h2>
+);
+
+export default EmployeeErrorMessage;
