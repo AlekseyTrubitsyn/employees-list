f7c64daaa69c63a4b051f052be05416b06562cb1
Aleksey Trubitsyn <at.8441451@yandex.ru>
2019-06-14 14:36:18 +0300

    build

---
+++ build/precache-manifest.4736411f60a172db70687e23aad71860.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbd355e89c934582b5c23ab9fd4bc913",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8a0503482026e83ce9b7",
+    "url": "./static/css/main.e2d21790.chunk.css"
+  },
+  {
+    "revision": "434854556b571b4aadab",
+    "url": "./static/js/2.7b765e87.chunk.js"
+  },
+  {
+    "revision": "8a0503482026e83ce9b7",
+    "url": "./static/js/main.a98f5902.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "38ee88a25d74cff6b29abfa42ebee67d",
+    "url": "./static/media/seamless_paper_texture.38ee88a2.png"
+  }
+]);
